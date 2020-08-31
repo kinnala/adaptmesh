010ba5e6b2e3bede165867bb0c6ea220cff27768
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-08-31 16:04:30 +0300

    Add a test

---
+++ tests/test_triangulate.py
@@ -0,0 +1,9 @@
+from adaptmesh import triangulate
+
+
+def test_square():
+    m = triangulate([(0., 0.),
+                     (1., 0.),
+                     (1., 1.),
+                     (0., 1.),])
+    assert m.t.shape[1] == 52
