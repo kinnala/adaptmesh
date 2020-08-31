fc73a56805c4c599dcf61c52abb44b28ec1f29a8
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-08-31 17:03:05 +0300

    Fix isort again

--- adaptmesh/tri/__init__.py
+++ adaptmesh/tri/__init__.py
@@ -5,7 +5,7 @@ __version__ = "0.3.1.dev0"
 __license__ = "MIT License"
 __author__ = "Martijn Meijers"
 
-from .delaunay import (ToPointsAndSegments,  # , polygon_as_points_and_segments
-                       triangulate)
+from .delaunay import ToPointsAndSegments  # , polygon_as_points_and_segments
+from .delaunay import triangulate
 
 __all__ = ["triangulate"]  # , "polygon_as_points_and_segments"]
