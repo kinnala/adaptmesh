436735a43df8f482ba4dfec35ce42fcc157ce666
Tom Gustafsson <tom.gustafsson@iki.fi>
2022-02-04 16:27:19 +0200

    fix flake8

--- tests/test_nonconvex.py
+++ tests/test_nonconvex.py
@@ -2,13 +2,15 @@ from adaptmesh import initial
 
 
 def test_nonconvex():
-    pts = [(0.0, 0.0),
-           (1.1, 0.0),
-           (1.2, 0.5),
-           (0.7, 0.6),
-           (2.0, 1.0),
-           (1.0, 2.0),
-           (0.5, 1.5),]
+    pts = [
+        (0.0, 0.0),
+        (1.1, 0.0),
+        (1.2, 0.5),
+        (0.7, 0.6),
+        (2.0, 1.0),
+        (1.0, 2.0),
+        (0.5, 1.5),
+    ]
 
     m = initial.cdt(pts)
     boundary_pts = set(map(tuple, m.p.T[m.boundary_nodes()]))
